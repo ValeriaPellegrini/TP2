95adf4f8bb2cc305eb1d062f12804a709e8e3e0b
Cesar M. <cesarmomberg@gmail.com>
2024-11-11 20:15:54 -0300

    "Cambios en el Heap, ahora sincroniza"

--- tp2/src/main/java/aed/Ciudad.java
+++ tp2/src/main/java/aed/Ciudad.java
@@ -6,6 +6,7 @@ public class Ciudad {
     private int id;
     private int gananciaTotal;
     private int perdidaTotal;
+    private int heapIndex = -1;
 
     public Ciudad(int id) {
         this.id = id;
@@ -17,7 +18,8 @@ public class Ciudad {
     public int getGananciaTotal() { return gananciaTotal; }
     public int getPerdidaTotal() { return perdidaTotal; }
     public int getSuperavit() { return gananciaTotal - perdidaTotal; }
-
+    public int getHeapIndex() { return heapIndex; }
+    public void setHeapIndex(int index) { this.heapIndex = index; }
 
     public void agregarPerdida(int perdida) {
         this.perdidaTotal += perdida;
