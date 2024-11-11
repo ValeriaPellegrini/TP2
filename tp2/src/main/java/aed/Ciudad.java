ef383fa26a667284b8f65bb6426b0b5df1027831
Rodrycue18 <rodrigosaucedo2002@gmail.com>
2024-11-11 20:52:28 -0300

    quiero pullear no me deja

--- tp2/src/main/java/aed/Ciudad.java
+++ tp2/src/main/java/aed/Ciudad.java
@@ -2,30 +2,30 @@ package aed;
 
 import java.util.Comparator;
 
-public class Ciudad {
-    private int id;
-    private int gananciaTotal;
-    private int perdidaTotal;
+    public class Ciudad {
+        private int id;
+        private int gananciaTotal;
+        private int perdidaTotal;
 
-    public Ciudad(int id) {
-        this.id = id;
-        this.gananciaTotal = 0;
-        this.perdidaTotal = 0;
-    }
+        public Ciudad(int id) {
+            this.id = id;
+            this.gananciaTotal = 0;
+            this.perdidaTotal = 0;
+        }
 
-    public int getId() { return id; }
-    public int getGananciaTotal() { return gananciaTotal; }
-    public int getPerdidaTotal() { return perdidaTotal; }
-    public int getSuperavit() { return gananciaTotal - perdidaTotal; }
+        public int getId() { return id; }
+        public int getGananciaTotal() { return gananciaTotal; }
+        public int getPerdidaTotal() { return perdidaTotal; }
+        public int getSuperavit() { return gananciaTotal - perdidaTotal; }
 
 
-    public void agregarPerdida(int perdida) {
-        this.perdidaTotal += perdida;
-    }
+        public void agregarPerdida(int perdida) {
+            this.perdidaTotal += perdida;
+        }
 
-    public void agregarGanancia(int ganancia) {
-        this.gananciaTotal += ganancia;
-    }
+        public void agregarGanancia(int ganancia) {
+            this.gananciaTotal += ganancia;
+        }
 
     public static Comparator<Ciudad> byMayorSuperavit() {
         return Comparator.comparingInt(Ciudad::getSuperavit).reversed()
