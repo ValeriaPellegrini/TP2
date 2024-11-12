845475176edc3536637b432d6b4150793f2f1d51
Rodrycue18 <rodrigosaucedo2002@gmail.com>
2024-11-11 21:11:25 -0300

    Arregle esta cagada

--- tp2/src/main/java/aed/Ciudad.java
+++ tp2/src/main/java/aed/Ciudad.java
@@ -7,16 +7,12 @@ public class Ciudad {
     private int gananciaTotal;
     private int perdidaTotal;
     private int heapIndex = -1;
-    public class Ciudad {
-        private int id;
-        private int gananciaTotal;
-        private int perdidaTotal;
 
-        public Ciudad(int id) {
-            this.id = id;
-            this.gananciaTotal = 0;
-            this.perdidaTotal = 0;
-        }
+    public Ciudad(int id) {
+        this.id = id;
+        this.gananciaTotal = 0;
+        this.perdidaTotal = 0;
+    }
 
     public int getId() { return id; }
     public int getGananciaTotal() { return gananciaTotal; }
@@ -25,13 +21,13 @@ public class Ciudad {
     public int getHeapIndex() { return heapIndex; }
     public void setHeapIndex(int index) { this.heapIndex = index; }
 
-        public void agregarPerdida(int perdida) {
-            this.perdidaTotal += perdida;
-        }
+    public void agregarPerdida(int perdida) {
+        this.perdidaTotal += perdida;
+    }
 
-        public void agregarGanancia(int ganancia) {
-            this.gananciaTotal += ganancia;
-        }
+    public void agregarGanancia(int ganancia) {
+        this.gananciaTotal += ganancia;
+    }
 
     public static Comparator<Ciudad> byMayorSuperavit() {
         return Comparator.comparingInt(Ciudad::getSuperavit).reversed()
